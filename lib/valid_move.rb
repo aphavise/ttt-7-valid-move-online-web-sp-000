920388c6d6fcc5a0f1b1de9766ee432c1ae3070a
Andrew P <andrewphaviseth@gmail.com>
2020-04-15 06:02:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 # code your #valid_move? method here
 
 def valid_move?(board,index)
-  # if board[index] == "" || board[index] == " " || board[index] =! nil 
   if position_taken?(board, index) || index > board.length()
       return false
     else
